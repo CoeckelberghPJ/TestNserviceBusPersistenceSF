2a3e9128103dc4ecdd5ea91fafa831a058bc6de7
Pieter Jan Coeckelbergh <PieterJan.Coeckelbergh@Katoennatie.com>
2021-11-16 10:26:14 +0100

    Add project files.

---
+++ Test.Service/TestData2.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Service
+{
+    public class TestData2 : ContainSagaData
+    {
+    }
+}
---
+++ Test.Service/TestSaga2.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Service
+{
+    public class TestSaga2 : Saga<TestData2>
+    {
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestData2> mapper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
